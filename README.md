691acc44c262b7ea3106e9dd624a918fd6a7fbca
Iago Calazans <iago.calazans@gmail.com>
2021-01-15 22:10:14 -0300

    implements nodejs PerformanceObserver as default http tracker

--- README.md
+++ README.md
@@ -1,14 +1,62 @@
+# Time Catcher
+
+## What it does?
+
+This module is a simple console printer that only works with Express and shows your SERVER INFO, EVENT LOOP INFO and REQUEST Process Time spent (duration) to end.
+
 # How to use
 
-### There are three important steps:
-1. Import it
-2. app.use it
-3. check your console.log out
+### There are four important steps:
+
+1. Install it
 
 ```
-import timeCatcher from 'timeCatcher'
+npm i time-catcher
+```
+
+2. Import it
+3. app.use it
+
+```
+import timeCatcher from 'timeCatcher';
+...
+
+app.use(timeCatcher);
+```
+
+```
+const timeCatcher = require('time-catcher').default;
+...
 
 app.use(timeCatcher)
 ```
+
+4. check your console out for logs!
+
+### Your output will be like this:
+
+```
+ ------  HTTP SERVER INFO  ------
+
+[CPU] 4x Intel(R) Core(TM) i7-7500U CPU @ 2.70GHz
+[RAM] 7.93 MB
+[RSS] 32.88 MB
+
+
+ ------  EVENT LOOP INFO  ------
+ < These values take into account all the time since the server was started >
+
+[% TIME] 2021-1-15 22:00:59 - 16%
+[TOTAL USE] 2021-1-15 22:00:59 - 5648ms
+
+
+ ------  REQUEST PROCESS INFO  ------
+
+[DURATION] 2021-1-15 22:00:59 - 449ms
+
+
+[CLOSE] 2021-1-15 22:00:59 - Request Process Ended
+```
+
 
 It is still too simple, but some features will come in future.
--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,24 +1,57 @@
 import { RequestHandler } from 'express'
 import chalk from 'chalk'
 import os from 'os'
+import { PerformanceObserver, performance } from 'perf_hooks'
 
-const timeCatcher: RequestHandler = (req, res, next): void => {
-  const time = Date.now()
-  const init = new Date().toLocaleString()
-  const log = console.log
-  const cpu = os.cpus()
-  const usage = process.memoryUsage().heapUsed / 1024 / 1024
-  const rss = process.memoryUsage().rss / 1024 / 1024
+const log = console.log
+const cpu = os.cpus()
+const usage = process.memoryUsage().heapUsed / 1024 / 1024
+const rss = process.memoryUsage().rss / 1024 / 1024
 
-  req.on('end', () => {
-    log(`[${chalk.cyan('START')}] ${chalk.gray(`${init}`)} - Request Process Start \n`)
-    log(`[${chalk.yellow('CPU')}] ${cpu.length}x ${cpu.shift()?.model}`)
+const obs = new PerformanceObserver(items => {
+  const gcs = items.getEntriesByType('gc')
+  const http = items.getEntriesByType('http')
+
+  const value = http[http.length - 1]
+
+  if (value) {
+    log(`\n\n ------ ${chalk.bgYellow(chalk.black(' HTTP SERVER INFO '))} ------ \n`)
+    log(`[${chalk.yellow('CPU')}] ${cpu.length}x ${cpu[0]?.model}`)
     log(`[${chalk.yellow('RAM')}] ${Math.round(usage * 100) / 100} MB`)
     log(`[${chalk.yellow('RSS')}] ${Math.round(rss * 100) / 100} MB`)
-    log(`\n[${chalk.green('INFO')}]: ${chalk.gray(`${new Date().toLocaleString()}`)} - Request took ${chalk.underline(`${Date.now() - time}ms`)} till ended on process.pid ${chalk.yellow(`${process.pid}`)} \n`)
-    log(`[${chalk.red('CLOSE')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - Request Process Ended`)
+    log(`\n\n ------ ${chalk.bgYellow(chalk.black(' EVENT LOOP INFO '))} ------ \n ${chalk.gray('< These values take into account all the time since the server was started >')} \n`)
+    log(`[${chalk.green('% TIME')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - ${Math.floor(performance.eventLoopUtilization().utilization * 100)}%`)
+    log(`[${chalk.green('TOTAL USE')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - ${Math.floor(performance.eventLoopUtilization().active)}ms`)
+    log(`\n\n ------ ${chalk.bgYellow(chalk.black(' REQUEST PROCESS INFO '))} ------ \n`)
+    log(`[${chalk.green('DURATION')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - ${Math.floor(value.duration)}ms`)
+    log(`\n\n[${chalk.red('CLOSE')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - Request Process Ended \n\n`)
+  }
+
+  gcs.map((gc) => {
+    if (gc.flags !== 0) {
+      log(`\n ------ ${chalk.bgYellow(chalk.black(' GARBAGE COLLECT INFO '))} ------ \n`)
+      log(`[${chalk.green('DURATION')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - ${Math.floor(gc.duration)}ms`)
+      log(`[${chalk.green('FLAGS')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - ${gc.flags}`)
+    }
+    return gc
   })
 
+  if (http.length > 10) {
+    obs.disconnect()
+    log(chalk.green('Reconnecting Observer'))
+  }
+})
+
+let init = false
+
+const timeCatcher: RequestHandler = (req, res, next): void => {
+  if (!init) {
+    req.destroy()
+    init = true
+  }
+
+  log(`[${chalk.cyan('START')}] ${chalk.gray(`${new Date().toLocaleString()}`)} - Request Process Start \n`)
+  obs.observe({ entryTypes: ['http', 'gc'], buffered: true })
   return next()
 }
 
